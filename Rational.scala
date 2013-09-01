3cebbdad0e2985a21a2599227324dd7136810f2c
Brett Lischalk <brett@brettlischalk.com>
2013-09-01 18:23:23 -0500

    Added fields to be used as accessors.  Added auxilary constructor.  Added gcd factoring.

---
+++ Rational.scala
@@ -0,0 +1,21 @@
+class Rational(n: Int, d: Int) {
+  require(d != 0)
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+  // field val denom: Int = d
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+  // Pre- condition
+  override def toString = numer +"/"+ denom
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+  def lessThan(that: Rational) =
+    this.numer * that.denom < that.numer * this.denom
+  def max(that: Rational) =
+    if (this.lessThan(that)) that else this
+  private def gcd(a: Int, b: Int): Int =
+    if (b==0) a else gcd(b, a % b)
+}
