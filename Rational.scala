46b8d09fac303b097051bf9f77105696ac6c0de4
Brett Lischalk <brett@brettlischalk.com>
2013-10-05 13:22:01 -0500

    Bringing up to date.

--- Rational.scala
+++ Rational.scala
@@ -5,17 +5,36 @@ class Rational(n: Int, d: Int) {
   val denom = d / g
   // field val denom: Int = d
   def this(n: Int) = this(n, 1) // auxiliary constructor
-  // Pre- condition
-  override def toString = numer +"/"+ denom
-  def add(that: Rational): Rational =
+  
+  def +(that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+  def +(i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+  def - (i: Int): Rational =
+    new Rational(numer * numer, denom * denom)
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+  def * (i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
   def lessThan(that: Rational) =
     this.numer * that.denom < that.numer * this.denom
   def max(that: Rational) =
     if (this.lessThan(that)) that else this
+  // Pre- condition
+  override def toString = numer +"/"+ denom
   private def gcd(a: Int, b: Int): Int =
     if (b==0) a else gcd(b, a % b)
 }
